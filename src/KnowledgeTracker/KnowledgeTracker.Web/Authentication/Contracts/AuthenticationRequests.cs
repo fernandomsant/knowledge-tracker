9c29da56caf3727df36b658c092a48a1c842bf32
fernando morais <fernando.morais0703@gmail.com>
2026-08-09 15:53:41 -0300

    fix(auth): bind validation to request properties

--- src/KnowledgeTracker/KnowledgeTracker.Web/Authentication/Contracts/AuthenticationRequests.cs
+++ src/KnowledgeTracker/KnowledgeTracker.Web/Authentication/Contracts/AuthenticationRequests.cs
@@ -2,12 +2,20 @@
 
 namespace KnowledgeTracker.Web.Authentication.Contracts;
 
-public sealed record RegisterRequest(
-    [property: Required, StringLength(256, MinimumLength = 1)] string Login,
-    [property: Required, StringLength(1024, MinimumLength = 1)] string Password
-);
+public sealed record RegisterRequest
+{
+    [Required, StringLength(256, MinimumLength = 1)]
+    public required string Login { get; init; }
 
-public sealed record LoginRequest(
-    [property: Required, StringLength(256, MinimumLength = 1)] string Login,
-    [property: Required, StringLength(1024, MinimumLength = 1)] string Password
-);
+    [Required, StringLength(1024, MinimumLength = 1)]
+    public required string Password { get; init; }
+}
+
+public sealed record LoginRequest
+{
+    [Required, StringLength(256, MinimumLength = 1)]
+    public required string Login { get; init; }
+
+    [Required, StringLength(1024, MinimumLength = 1)]
+    public required string Password { get; init; }
+}
